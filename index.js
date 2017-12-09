d3f66cff6cfce757ae924dfb0d653fe6922e6f75
jon atlas <JonAtlas@jons-MacBook-Air-2.local>
2017-12-08 19:23:18 -0500

    revisions

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
+
+    $("form").submit(function(event){
+      event.preventDefault();
+      setTimeout(
+       function() {
+          /* clear elements on form submit */
+           $(".thank-you").css("display", "block");
+           $(".to-hide").css("display", "none");
+       },
+       500);
+    });
+
 });
